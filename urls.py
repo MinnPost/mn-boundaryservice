e3bdbe1d0bce17f54c386c0103f970f7cc92c008
Michael Mulley <michael@michaelmulley.com>
2011-12-16 23:55:34 -0500

    Repair admin URL

--- urls.py
+++ urls.py
@@ -13,11 +13,8 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    (r'^admin/(.*)', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 
     (r'', include('boundaryservice.urls')),
     (r'', include('finder.urls')),
