0d33d5c7112590f1a5ba8c34fc09fe2bc08a116b
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-05-09 15:57:57 -0500

    Adding deployment sscripts to folder.

---
+++ deployment/README.md
@@ -0,0 +1,5 @@
+These files are used for deployment.  They are hard-coded at the
+moment.  Sorry.
+
+For more details on install and deployment, see:
+https://github.com/MinnPost/mn-boundaryservice/wiki/Deployment-and-Install
---
+++ deployment/gunicorn_startup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -e
+LOGFILE=/var/log/gunicorn/boundaryservice.log
+LOGDIR=$(dirname $LOGFILE)
+NUM_WORKERS=3
+USER=ubuntu
+
+cd /home/ubuntu/django-projects/mn-boundaryservice
+source boundaryservice/bin/activate
+test -d $LOGDIR || mkdir -p $LOGDIR
+exec boundaryservice/bin/gunicorn_django -w $NUM_WORKERS \
+    --user=$USER --log-level=debug \
+    --log-file=$LOGFILE 2>>$LOGFILE
