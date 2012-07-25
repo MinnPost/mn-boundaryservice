cb64e1742439d7feb6b5b16f26ca424475ee6f27
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-07-25 18:02:39 -0500

    Changed to mapquest tiles.

--- finder/static/js/finder.js
+++ finder/static/js/finder.js
@@ -45,10 +45,13 @@ function init_map(lat, lng) {
             center: ll,
         });
 
-        tiles = new L.TileLayer("http://mt1.google.com/vt/lyrs=m@155000000&hl=en&x={x}&y={y}&z={z}&s={s}", {
-            maxZoom: 17,
-            attribution: "Map data is Copyright Google, 2011"
-        });
+        var mapquestUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png';
+        var subDomains = ['otile1','otile2','otile3','otile4'];
+        var mapquestAttrib = 'Data, imagery and map information provided by ' +
+          '<a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, ' +
+          '<a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> ' +
+          'and contributors.';
+        var tiles = new L.TileLayer(mapquestUrl, {maxZoom: 18, attribution: mapquestAttrib, subdomains: subDomains});
         
         map.addLayer(tiles);
     }
